bf04b3c768f7bca1880ae77bf84feb7de19d98e8
Erik Krieg <erik.c.krieg@gmail.com>
2017-07-23 20:05:49 -0400

    updates LinkList to load from GQL

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -14,18 +14,13 @@ const ALL_LINKS_QUERY = gql`
 
 class LinkList extends Component {
     render() {
-        const linksToRender = [
-            {
-                id: '1',
-                description: 'The Coolest GraphQL Backend 😎',
-                url: 'https://www.graph.cool'
-            },
-            {
-                id: '2',
-                description: 'The Best GraphQL Client',
-                url: 'http://dev.apollodata.com/'
-            }
-        ]
+        if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+            return <div>Loading</div>
+        }
+        if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+            return <div>Error</div>
+        }
+        const linksToRender = this.props.allLinksQuery.allLinks
         return (
             <div>
                 {linksToRender.map(link => <Link key={link.id} link={link} />)}
