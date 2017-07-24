7a7fefff35ab23be6e24cdb359ec671904e17de6
Erik Krieg <erik.c.krieg@gmail.com>
2017-07-23 20:50:12 -0400

    refactors DOM for app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
@@ -12,11 +12,11 @@ const networkInterface = createNetworkInterface({
 const client = new ApolloClient({ networkInterface })
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <BrowserRouter>
+    <BrowserRouter>
+        <ApolloProvider client={client}>
             <App />
-        </BrowserRouter>
-    </ApolloProvider>,
+        </ApolloProvider>
+    </BrowserRouter>,
     document.getElementById('root')
 )
 registerServiceWorker()
