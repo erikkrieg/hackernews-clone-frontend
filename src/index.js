2b6da8c80402818e94bc596bf9b585225b1643b2
Erik Krieg <erik.c.krieg@gmail.com>
2017-07-23 20:48:29 -0400

    adds BrowserRouter to enable routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
@@ -12,7 +13,9 @@ const client = new ApolloClient({ networkInterface })
 
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </ApolloProvider>,
     document.getElementById('root')
 )
